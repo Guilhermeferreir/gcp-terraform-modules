934e609305a7d081f2327c4b72c76093317769ff
Guilherme Ferreira <83267524+Guilhermeferreir@users.noreply.github.com>
2024-04-19 10:29:34 -0300

    added artifact repository

---
+++ artifact_repository/variables.tf
@@ -0,0 +1,15 @@
+variable "location" {
+  type = string
+}
+
+variable "repository_id" {
+  type = string
+}
+
+variable "description" {
+  type = string
+}
+
+variable "format" {
+  type = string
+}
